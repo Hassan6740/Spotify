7199d0fb0a1e13b3d6080fd1308fcba8677874f9
Hassan <mhassanjm78@gmail.com>
2024-09-10 05:03:08 +0500

    javascript and library

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 console.log("Lets write JavaScript");
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    console.log(response);
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = []
+    for (let i = 0; i < as.length; i++) {
+        const element = as[i]
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1].split(".mp3")[0])
+        }
+    }
+    return songs;
+}
+
+async function main() {
+    let songs = await getSongs();
+    console.log(songs);
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + 
+        `<li>
+            <img class="invert" src="music.svg" alt="">
+            <div class="info">
+                <div>${song.replaceAll("%20", " ")}</div>
+                <div>JD</div>
+            </div>
+            <div class="playNow">
+                <span>Play Now</span>
+                <img class="invert" src="play.svg" alt="">
+            </div>
+             
+        </li>`;
+    }
+    var audio = new Audio(songs[0])
+    // audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    });
+}
+main();
